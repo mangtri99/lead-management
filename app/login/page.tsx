03981e0c31fd5db2b144d1a506264b9c968b3bb7
Mang Tri <mangtri93@gmail.com>
2023-12-27 13:36:15 +0800

    feat: add max width login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -41,78 +41,82 @@ export default function Page() {
       </div>
 
       {/* Form Login */}
-      <div className="lg:w-2/5 w-full flex flex-col h-full items-center justify-center px-4">
-        <Image
-          src="/img/logo.svg"
-          width={0}
-          height={0}
-          className="max-w-sm w-1/2"
-          alt="Picture of the author"
-        />
+      <div className="lg:w-2/5 w-full flex flex-col h-full items-center justify-center">
+        <div className="max-w-sm">
+          <div className="flex justify-center">
+            <Image
+              src="/img/logo.svg"
+              width={0}
+              height={0}
+              className="w-full"
+              alt="Picture of the author"
+            />
+          </div>
 
-        <div className="mt-10 text-center w-full">
-          <h1 className="text-3xl font-bold">GX APP 2.0</h1>
-          <p className="text-sm">Sign in to your account below</p>
-        </div>
+          <div className="mt-10 text-center w-full">
+            <h1 className="text-3xl font-bold">GX APP 2.0</h1>
+            <p className="text-sm">Sign in to your account below</p>
+          </div>
 
-        <div className="lg:w-2/3 w-full mt-6">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              {/* Email */}
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="email"
-                        placeholder="Type your email here"
-                        {...field}
+          <div className=" w-full mt-6">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                {/* Email */}
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="email"
+                          placeholder="Type your email here"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* Password */}
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="Type your password here"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* Remember */}
+                <FormField
+                  control={form.control}
+                  name="remember"
+                  render={({ field }) => (
+                    <FormItem className="flex items-center">
+                      <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
                       />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* Password */}
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="password"
-                        placeholder="Type your password here"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* Remember */}
-              <FormField
-                control={form.control}
-                name="remember"
-                render={({ field }) => (
-                  <FormItem className="flex items-center">
-                    <FormControl>
-                    <Checkbox
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                    />
-                    </FormControl>
-                    <FormLabel className="ml-2 !mt-0">Remember me</FormLabel>
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Submit</Button>
-            </form>
-          </Form>
+                      </FormControl>
+                      <FormLabel className="ml-2 !mt-0">Remember me</FormLabel>
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+          </div>
         </div>
       </div>
     </div>
